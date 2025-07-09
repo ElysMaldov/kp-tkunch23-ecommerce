4fc6dfd391c6474bbfb01a6f35ce84a821da3d86
Elys Maldov <maldovelys@gmail.com>
2025-07-09 15:28:25 +0700

    feat: add authorization for home and auth pages

--- src/app/(frontend)/auth/login/page.tsx
+++ src/app/(frontend)/auth/login/page.tsx
@@ -1,5 +1,13 @@
 import LoginForm from "@/components/LoginForm";
+import { authorizeUser } from "@/lib/actions/authorize-user";
+import { redirect } from "next/navigation";
 
-export default async function HomePage() {
-  return <LoginForm />;
+export default async function LoginPage() {
+  const user = await authorizeUser();
+
+  if (user) {
+    redirect("/");
+  } else {
+    return <LoginForm />;
+  }
 }
