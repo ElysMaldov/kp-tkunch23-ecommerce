59f48ffcfd0018edbd9dd9955aae24c16baae2c2
Elys Maldov <maldovelys@gmail.com>
2025-07-11 16:31:37 +0700

    feat: setup midtrans payment creation

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import PaymentFormDialog from "@/components/PaymentFormDialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { createMidtransTransaction } from "@/lib/actions/create-midtrans-transaction";
 import { Heart, RotateCcw, Share2, Shield, ShoppingCart, Star, Truck } from "lucide-react";
 import Image from "next/image";
 import { ReactNode } from "react";
@@ -16,6 +19,24 @@ interface ProductPageProps {
 export default function ProductPage({ name, price, imageUrl, description }: ProductPageProps) {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <Button
+        onClick={async () => {
+          const transaction = await createMidtransTransaction(
+            10_000,
+            "Reyhan",
+            "reyhan@gmail.com",
+            "+628724783",
+          );
+
+          console.log(transaction);
+
+          // @ts-expect-error
+          window.open(transaction.redirect_url, "_self");
+        }}
+      >
+        Test Midtrans function
+      </Button>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12">
         {/* Product Image Section */}
         <div className="space-y-4">
